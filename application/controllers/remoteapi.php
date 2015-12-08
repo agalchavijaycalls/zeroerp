3411a61f00db32485e0f8e2a8971e3f729996163
ankit <dev2@junctiontech.in>
2015-12-08 12:10:36 +0530

    test for json

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -145,7 +145,7 @@ class Remoteapi{
 		if($CONNECTION)
 		{
 			$data=json_decode($_POST['projectData']);//print_r($data);die;
-			$UserId=$data->user_id;
+			$UserId=$data->user_id;print_r($data->project_List);die;
 			$ProjectList=$data->project_List;
 			print_r($ProjectList->expense_list);die;
 			$ProjectId=$data->project_id;
