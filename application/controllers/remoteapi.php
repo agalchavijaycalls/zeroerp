86b736c63bc1385d43568611f7b45d3e88b2ed29
ankit <dev2@junctiontech.in>
2015-12-15 16:36:32 +0530

    test

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -13,8 +13,8 @@ class Remoteapi extends CI_Controller{
 	{
 		//error_reporting(0);
 		
-		$data=json_decode($_POST['employeeData']);print_r($data);die;
-		$imei=$data->employeeIMEI;
+		$data=json_decode($_POST['employeeData']);//print_r($data);die;
+		$imei=$data->employeeIMEI;echo $imei;die;
 		if(isset($imei) && !empty($imei) && isset($data->employeeOrganizationName) && !empty($data->employeeOrganizationName))
 		{
 			//if(!isset($_GET['result']) && empty($_GET['result']))
