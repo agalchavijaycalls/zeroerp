b6608fce1cafa0f872aea10fcd6ae8ec826a2c9a
ankit <dev2@junctiontech.in>
2015-12-19 15:09:09 +0530

    update employee id in registration

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -140,7 +140,7 @@ class Remoteapi extends CI_Controller{
 				$count=mysqli_num_rows($result);
 				if(isset($count) && !$count > 0 )
 				{
-					$query= "INSERT INTO newregistration (name,number,password,imei,device,status,created_by,created_on,updated_by,updated_on) VALUES('".$data->employeeName."','".$data->employeeMobileNumber."','".$data->employeePassword."','".$data->employeeIMEI."','androide','new','".$data->employeeName."','".date('d-m-Y')."','','')"; //echo $query; die;
+					$query= "INSERT INTO newregistration (employee_id,name,number,password,imei,device,status,created_by,created_on,updated_by,updated_on) VALUES('".$data->employeeId."','".$data->employeeName."','".$data->employeeMobileNumber."','".$data->employeePassword."','".$data->employeeIMEI."','androide','new','".$data->employeeName."','".date('d-m-Y')."','','')"; //echo $query; die;
 					$sql=mysqli_query($new_connection,$query);
 					if($sql)
 					{
