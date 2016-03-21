920b340c1d96c7043c303d11499fdd7169ac443e
JUNCTION TECH <JUNCTION TECH@jayadevi>
2016-03-21 16:11:37 +0530

    d

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -510,7 +510,7 @@ function loanRegistration()
   if($CONNECTION!=='')
   {
    $data1=json_decode($_POST['registration_info'],true);
-   $data = json_decode($data1['mValues']);
+   $data = json_decode($data1['mValues'],true);
  
    
    $query= "select * from loan_registration where emailId='".$data->LoanApp_emailId."'";
