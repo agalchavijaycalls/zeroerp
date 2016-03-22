0adad4ef5932e5c73a8a5c591c025e8476b44bdf
JUNCTION TECH <JUNCTION TECH@jayadevi>
2016-03-22 16:34:58 +0530

    d

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -639,10 +639,11 @@ function loanRegistration()
 			$LoanApp_dislike = $data['LoanApp_dislike'];
 			$LoanApp_dateTime = $data['LoanApp_dateTime'];			
 					
-				
+			print_r($data);die;
+			
 				$query= "INSERT INTO loanApplication (emailId,like,dislike,duration,status,ammount,type,date_time) VALUES('$LoanApp_emailId','$LoanApp_like','$LoanApp_dislike','$LoanApp_duration','$LoanApp_status','$LoanApp_ammount','$LoanApp_type,'$LoanApp_dateTime')"; //echo $query; die;
 				$sql=mysqli_query($CONNECTION,$query);
-				print_r($sql);die;
+				
 				if($sql)
 				{
 					$result=array(
