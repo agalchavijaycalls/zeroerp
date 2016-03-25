d69dc0729d634c89b96ff7f08e211d24ae22a4d2
JUNCTION TECH <JUNCTION TECH@jayadevi>
2016-03-25 14:38:59 +0530

    trh

--- application/controllers/remoteapi.php
+++ application/controllers/remoteapi.php
@@ -632,7 +632,7 @@ function loanRegistration()
  		$dateTime = $_POST['dateTime'];
  		$like_dislike_status = $_POST['like_dislike_status'];		
 
- 		$query= "select * from seek_reference,loanapplication where seek_reference.emailId='$referralEmailId' and seek_reference.date_time='$dateTime' and loanapplication.emailId=seek_reference.emailId and seek_reference.date_time=loanapplication.date_time";
+ 		$query= "select * from seek_reference,loanapplication where seek_reference.emailId='$referralEmailId' and 	referalEmailId.emailId ='$emailId' and seek_reference.date_time='$dateTime' and loanapplication.emailId=seek_reference.emailId and seek_reference.date_time=loanapplication.date_time";
  		$sql=mysqli_query($CONNECTION,$query);
  		
  		
