a6534b355e9018fd2c64df98cc72c4d7926f9dd8
Rohit Singh Thakur <rohitjunctiontech>
2016-10-03 18:44:40 +0530

    j

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -11,23 +11,13 @@ class Login extends CI_Controller {
 		
 	}
 	
-	
+	public function index(){
+		echo"rohit";
+	}
 	
 
-				 /* login view */
-	public function login_view($id=false,$code=false)
-	{      
-             //   if($code)
-             //   {
-             //    $role_update=$this->data['role_update']=$this->login_model->role_update($id); 
-              //  }
-		//$list_organization = $this->data['list_organization'] = $this->organization_model->list_organization();
-		$this->parser->parse('include/header',$this->data);
-		$this->load->view('login',$this->data);//login page view
-		//$this->parser->parse('include/footer',$this->data);
-	}
+
 				
-               				/*android code */
 	
 	
 }
