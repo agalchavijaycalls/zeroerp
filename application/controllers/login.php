9ca3fbdc72c1560b72ea3114709dd1ed29eba1ac
Rohit Singh Thakur <rohitjunctiontech>
2016-10-03 18:17:54 +0530

    k

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -142,6 +142,7 @@ class Login extends CI_Controller {
              //    $role_update=$this->data['role_update']=$this->login_model->role_update($id); 
               //  }
 		//$list_organization = $this->data['list_organization'] = $this->organization_model->list_organization();
+		print_r("hello");die;
 		$this->parser->parse('include/header',$this->data);
 		$this->load->view('login',$this->data);//login page view
 		//$this->parser->parse('include/footer',$this->data);
